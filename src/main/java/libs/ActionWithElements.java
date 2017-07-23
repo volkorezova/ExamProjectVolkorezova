04f37752be8632864a6be61b94ea64d305e5e57a
Tatyana Volkorezova <tatyana.volkorezova@onix-systems.com>
2017-07-23 11:24:40 +0300

    create sceleton of further framework: - added general class ActionsWithElements - added class ParentPage - added class ZapisPage - added class ParentTest with initialization of webdriwer (setUp nd tearDown methods)

---
+++ src/main/java/libs/ActionWithElements.java
@@ -0,0 +1,7 @@
+package libs;
+
+/**
+ * Created by tatyanavolkorezova on 23.07.17.
+ */
+public class ActionWithElements {
+}
--- src/main/java/pages/ParentPage.java
+++ src/main/java/pages/ParentPage.java
@@ -3,5 +3,5 @@ package pages;
 /**
  * Created by tatyanavolkorezova on 23.07.17.
  */
-public class ZapisPage extends ParentPage {
+public class ParentPage {
 }
